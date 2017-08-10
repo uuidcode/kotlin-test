d2fefd7e10490a351cb4912387cb18a86cfb96d5
uuidcode <uuidcode@gmail.com>
2017-08-10 17:54:29 +0900

    HelloKtSpek

---
+++ src/test/java/com/github/uuidcode/kotlin/HelloKtSpek.kt
@@ -0,0 +1,19 @@
+package com.github.uuidcode.kotlin
+
+import org.assertj.core.api.Assertions
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.on
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+
+@RunWith(JUnitPlatform::class)
+class HelloKtSpek : Spek({
+    describe("a calculator") {
+        val hello = Hello()
+
+        on("test") {
+            Assertions.assertThat(hello.test()).isEqualTo("Hello, World!")
+        }
+    }
+})
