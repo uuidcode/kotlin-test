7096c38a5bd66e00421e8b8725696d5fae3cc337
uuidcode <uuidcode@gmail.com>
2017-08-16 18:27:05 +0900

    Death

--- src/test/java/com/github/uuidcode/kotlin/BlockTest.kt
+++ src/test/java/com/github/uuidcode/kotlin/BlockTest.kt
@@ -12,6 +12,13 @@ class BlockTest {
         return block(name)
     }
 
+
+    var sum = {
+        x: Int, y: Int -> x + y
+    }
+
+    var add : ((Int, Int) -> Int)? = null
+
     @Test
     fun executeTest() {
         execute {
@@ -20,6 +27,7 @@ class BlockTest {
 
         var result = execute({ it + it }, "Hello")
         assertThat(result).isEqualTo("HelloHello")
+        assertThat(sum(1, 2)).isEqualTo(3)
     }
 
     @Test
@@ -36,4 +44,37 @@ class BlockTest {
         1.greetInt()
     }
 
+
+    @Test
+    fun die() {
+        var person = Person(10, "uuid", "code")
+
+        var death = person.die {
+            it.at = 100
+        }
+
+        assertThat(death.from).isEqualTo("old")
+        assertThat(death.at).isEqualTo(100)
+
+        var deathFun : Person.(Death) -> Unit = {
+            it.at = 1000
+        }
+
+        death = person.die(deathFun)
+
+        assertThat(death.from).isEqualTo("old")
+        assertThat(death.at).isEqualTo(1000)
+
+        person.deathFun(Death("accident", 3))
+    }
+}
+
+fun <T : Person> T.die(body : T.(Death) -> Unit) : Death {
+    val death = Death("old")
+
+    death.apply {
+        body(this)
+    }
+
+    return death
 }
